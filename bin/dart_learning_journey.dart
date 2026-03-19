2d5b0537dc896ee8ec5eb3ddfd2114c8455324d1
bastqq <bastqq@gmail.com>
2026-03-19 18:49:06 +0200

    les4_p1f

--- bin/dart_learning_journey.dart
+++ bin/dart_learning_journey.dart
@@ -1,4 +1,4 @@
-import 'package:dart_learning_journey/dart_learning_journey.dart' as dart_learning_journey;
+import 'package:dart_learning_journey/lesson_04/dart_learning_journey.dart' as dart_learning_journey;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_learning_journey.calculate()}!');
