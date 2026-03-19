756b454b8f3437f9b9ead3d8cac93990ce8bce42
bastqq <bastqq@gmail.com>
2026-03-19 18:55:20 +0200

    l4_p5

--- lib/lesson_04/dart_learning_journey.dart
+++ lib/lesson_04/dart_learning_journey.dart
@@ -62,4 +62,8 @@ print( myRecord.a);
 /// Завдання 5: Nullable (опціонально)
 /// Створи змінну String? nickname — може бути null або ім'я.
 /// Виведи її через ??: якщо null — виведи "Немає", інакше — значення.
-void nullable() {}
+void nullable() {
+String? nickname ;
+print(nickname?? 'Немає');
+
+}
