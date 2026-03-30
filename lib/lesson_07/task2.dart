82e35f1ef37125f3d5de5ab6bb87a4a844f0258c
bastqq <bastqq@gmail.com>
2026-03-30 23:24:51 +0300

    feat(DARD-07): complited 2 part

---
+++ lib/lesson_07/task2.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:dart_learning_journey/lesson_07/homework/names_deepseek.dart';
+import 'package:dart_learning_journey/lesson_07/homework/names_gpt.dart';
+
+void main() {
+
+final setPresentInBoth = ukrainianNamesDeepseek.intersection(ukrainianNamesGPT);
+print(setPresentInBoth.length);
+
+final inUkrainianNamesGPT = ukrainianNamesGPT.difference(ukrainianNamesDeepseek);
+print(inUkrainianNamesGPT);
+
+final inUkrainianNamesDeepseek = 
+ukrainianNamesDeepseek.difference(inUkrainianNamesGPT);
+print(inUkrainianNamesDeepseek);
+}
