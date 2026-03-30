960dcbdcbe1068537f281d5b25a9057da90c131a
bastqq <bastqq@gmail.com>
2026-03-31 00:26:45 +0300

    feat(DARD-07): complited 3 part

---
+++ lib/lesson_07/task3.dart
@@ -0,0 +1,23 @@
+import 'package:mocky/mocky.dart';
+
+
+void main(){
+
+final randomNouns =  Mocky.words(100); 
+print(randomNouns);
+
+final nounsMap = Map.fromEntries(
+  randomNouns.map((word) => MapEntry(word, word.length)),
+);
+print(nounsMap);
+
+
+ final nounsMapFiltered = {};
+
+nounsMap.forEach((key, value) {
+  if (key.length%2 == 0) {
+    nounsMapFiltered[key] = value;
+  }
+});
+print(nounsMapFiltered);
+}
